c397fb9b0538af310c837ddaae79283596f6e045
taniashahida-dev <taniashahida993@gmail.com>
2026-05-02 14:11:16 +0600

    fixe some minor problem

--- src/lib/data.jsx
+++ src/lib/data.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const getdata = async () => {
-    const  res = await fetch('http://localhost:3000/data/courses.json',{cache: "no-store"})
+    const  res = await fetch('/data/courses.json',{cache: "no-store"})
 
     return res.json()
 };
